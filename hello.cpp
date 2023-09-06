28926768f50ff1b6dd87e71118c3866d16106592
Kamoliddin Soliev <69765410+KamoliddinS@users.noreply.github.com>
2023-09-06 16:26:40 +0500

    smal fix

--- hello.cpp
+++ hello.cpp
@@ -1,13 +1,30 @@
 // 020-TestCase-2.cpp
 // Source: https://github.com/catchorg/Catch2/blob/master/examples/020-TestCase-2.cpp
-#define CATCH_CONFIG_MAIN
-#include "catch.hpp"
+// #define CATCH_CONFIG_MAIN
+// #include "catch.hpp"
 
-std::string HelloWorld() {
+// std::string HelloWorld() {
     
-    return "Hello, World!";
+//     return "Hello, World!";
+// }
+
+// TEST_CASE( "Hello, World!", "[lesson:1]" ) {
+//     REQUIRE( HelloWorld() == "Hello, World!" );
+// }
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+std::string ReverseString(const std::string& input) {
+    std::string reversed = input;
+    std::reverse(reversed.begin(), reversed.end());
+    return reversed;
 }
 
-TEST_CASE( "Hello, World!", "[lesson:1]" ) {
-    REQUIRE( HelloWorld() == "Hello, World!" );
+int main() {
+    std::string input;
+    std::getline(std::cin, input); // Read the entire line of input, including spaces
+    std::string reversed = ReverseString(input);
+    std::cout << reversed << std::endl;
+    return 0;
 }
